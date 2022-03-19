01cdff92f139c957098784c9461c50257c5471c5
liviu-moraru <liviu.moraru@gmail.com>
2022-03-19 12:24:33 +0200

    03-getting-started

--- 03-getting-started/examples/09-multiple-generic-types/main.go
+++ 03-getting-started/examples/09-multiple-generic-types/main.go
@@ -23,13 +23,31 @@ import (
 	"fmt"
 )
 
+// Composite constraints
+// Numeric expresses a type constraint satisfied by any numeric type.
+type Numeric interface {
+	~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 |
+		~int | ~int8 | ~int16 | ~int32 | ~int64 |
+		~float32 | ~float64 |
+		~complex64 | ~complex128
+}
+
+type id int64
+
 // Sum returns the sum of the provided arguments.
-func Sum[T any](args ...T) T {
+func Sum[T Numeric](args ...T) T {
 	var sum T
 	for i := 0; i < len(args); i++ {
 		sum += args[i]
 	}
 	return sum
+}
+
+type SumFn[T Numeric] func(...T) T
+
+// PrintIDAndSum prints the provided ID and sum of the given values to stdout.
+func PrintIdAndSum[T ~string, K Numeric](id T, sum SumFn[K], values ...K) {
+	fmt.Printf("%s has a sum of %v\n", id, sum(values...))
 }
 
 func main() {
@@ -38,4 +56,7 @@ func main() {
 	fmt.Println(Sum([]uint32{1, 2, 3}...))
 	fmt.Println(Sum([]float64{1.1, 2.2, 3.3}...))
 	fmt.Println(Sum([]complex128{1.1i, 2.2i, 3.3i}...))
+	fmt.Println(Sum([]id{10, 11, 12}...))
+	fmt.Println(Sum[float64](1, 2, 3.0))
+	PrintIdAndSum("0001", Sum[complex128], 10, 20, 30.2)
 }
